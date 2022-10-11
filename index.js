96684b6069e4b57b28a8494e475701262d74ad00
EcaCosca <enriquecoscarelli@gmail.com>
2022-10-11 13:20:21 +0200

    Mark as done

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-const handleChange = (e) => {
-    // console.log(e)
-    document.getElementById('ordered-list').innerHTML += `<li onclick="markAsDone(this)">${e}</li>`
+const addBtn = document.querySelector('.add-btn')
+
+
+
+
+
+// CHANGE BACKGROUND OF THIS ITEM SO IT'S MARKED AS DONE OR VICEVERSA
+
+const markAsDone = (e) => { 
+    // IF STATEMENT 
+    // if(!e.style.backgroundColor){
+    //     e.style.backgroundColor = 'red'
+    // }else{
+    //     e.style.backgroundColor = ''
+    // }
+
+    // TERNARY OPERATOR 
+    !e.style.backgroundColor ? e.style.backgroundColor = 'red' : e.style.backgroundColor = '' 
 }
 
 
-const handleSubmit = (e) => {
-    // e.preventDefault()
+// CLICK EVENT FOR SUBMITING A NEW ITEM IN A TO DO LIST
 
-    alert("submited")
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("submit")
+})
+
+// ADDING SOMETHING TO THE LIST
+
+const listItemCreator = (text) => {
+    document.getElementById('ordered-list').innerHTML += `<li onclick="markAsDone(this)">${text}</li>`
 }
-
-const markAsDone = (e) => { !e.style.backgroundColor ? e.style.backgroundColor = 'red' : e.style.backgroundColor = '' }
